3b718219ef6acd1f2a9d3a3ca99497ac62484723
mrfury0 <hannanfaryad@gmail.com>
2021-03-02 22:10:45 +0500

    Diving into flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('I am Hannan'),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/a.jpg'),
+        ),
+      ),
+    ),
+  ));
+}
